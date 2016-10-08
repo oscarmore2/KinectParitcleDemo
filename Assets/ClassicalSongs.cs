a97b958172b7249f3c4cfa1d9bc05d64f80a3971
oscar <mo.zhifen@whaley.cn>
2016-10-08 16:38:33 +0800

    Add Songs data model

---
+++ Assets/ClassicalSongs.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ClassicalSongs {
+	[System.Serializable]
+	public class Segment
+	{
+		public int Tampo;
+		public string startingTone;
+		public string endingTone;
+		public AudioClip Clip;
+	}
+
+	public enum RhythmType
+	{
+		Adagio, Moderato,  Allegro 
+	}
+
+	public string Name;
+	public int AvargeTampo;
+	public string Tone;
+	public Segment[] SongSegment;
+}
